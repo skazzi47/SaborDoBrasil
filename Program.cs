4e7e51c948729728694cf6d3fcb48fd61bc92607
Guilherme Mauricio <guilhermemauricio219@gmail.com>
2025-05-08 08:07:10 -0300

    Criação do projeto Sabor do Brasil

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); // Serve index.html por padrão
+app.UseStaticFiles(); // Permite servir arquivos da pasta wwwroot
+
+app.MapGet("/index", async context =>
+{
+    await context.Response.SendFileAsync("wwwroot/index.html");
+});
+
+// app.MapGet("/", () => "Hello World!");
+
+app.Run();
